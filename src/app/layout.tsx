22d1ab4e305cb29be1ecab8ef8534e8339c57379
Milind Mishra <milind.mishra4@gmail.com>
2024-09-30 06:16:17 +0530

    feat: update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Toaster richColors position="top-center" closeButton />
+        <Toaster richColors position="top-center" />
         {children}
       </body>
     </html>
--- src/components/json-visualizer.tsx
+++ src/components/json-visualizer.tsx
@@ -2,8 +2,10 @@
 
 import React, { useState, useEffect } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
 import { JsonInput } from "@/components/json-input";
 import { JsonViewer } from "@/components/json-viewer";
+import { Github } from "lucide-react";
 
 export function JsonVisualizer() {
   const [jsonInput, setJsonInput] = useState("");
@@ -29,15 +31,35 @@ export function JsonVisualizer() {
         onValueChange={setActiveTab}
         className="flex-grow flex flex-col"
       >
-        <TabsList className="justify-start">
-          <TabsTrigger value="input">Input</TabsTrigger>
-          <TabsTrigger value="viewer">Viewer</TabsTrigger>
-        </TabsList>
-        <div className="flex-grow">
-          <TabsContent value="input" className="h-full">
+        <div className="bg-gray-100 px-4 py-2 flex items-center justify-between shadow-sm">
+          <div className="flex items-center space-x-4">
+            <h1 className="text-sm font-medium text-gray-800">
+              JSON Visualizer
+            </h1>
+            <TabsList>
+              <TabsTrigger value="input">Input</TabsTrigger>
+              <TabsTrigger value="viewer">Viewer</TabsTrigger>
+            </TabsList>
+          </div>
+          <Button
+            variant="ghost"
+            size="xs"
+            onClick={() =>
+              window.open(
+                "https://github.com/thatbeautifuldream/json-visualizer",
+                "_blank"
+              )
+            }
+          >
+            <Github className="w-4 h-4 mr-2" />
+            thatbeautifuldream/json-visualizer
+          </Button>
+        </div>
+        <div className="flex-grow flex flex-col">
+          <TabsContent value="input" className="flex-grow p-4">
             <JsonInput jsonInput={jsonInput} setJsonInput={setJsonInput} />
           </TabsContent>
-          <TabsContent value="viewer" className="h-full">
+          <TabsContent value="viewer" className="flex-grow p-4">
             <JsonViewer parsedJson={parsedJson} error={error} />
           </TabsContent>
         </div>
