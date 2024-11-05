62169fd22f6adac7cd0b02ae69f371ce8b3a9675
Milind Mishra <milind.mishra4@gmail.com>
2024-11-05 08:35:00 +0530

    feat: make explore and stats page for admin only

--- src/app/(application)/explore/page.tsx
+++ src/app/(application)/explore/page.tsx
@@ -1,4 +1,7 @@
 import { ExploreGrid } from "@/components/explore-grid";
+import { env } from "@/env";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function ExplorePage({
   params,
@@ -7,6 +10,13 @@ export default async function ExplorePage({
     page: number;
   }>;
 }) {
+  const cookieStore = await cookies();
+  const adminKey = cookieStore.get("ADMIN_KEY")?.value;
+
+  if (!adminKey || adminKey !== env.ADMIN_KEY) {
+    redirect("/");
+  }
+
   const page = (await params).page ?? 1;
   return (
     <div className="container mx-auto py-8">
--- src/app/(application)/stats/page.tsx
+++ src/app/(application)/stats/page.tsx
@@ -1,6 +1,16 @@
 import { StatsCard } from "@/components/stats-card";
+import { env } from "@/env";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function StatsPage() {
+export default async function StatsPage() {
+  const cookieStore = await cookies();
+  const adminKey = cookieStore.get("ADMIN_KEY")?.value;
+
+  if (!adminKey || adminKey !== env.ADMIN_KEY) {
+    redirect("/");
+  }
+
   return (
     <div className="container mx-auto py-8">
       <StatsCard />
