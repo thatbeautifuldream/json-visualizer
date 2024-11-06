9650c6c51f8e24ff40ef79b0a844ae1bd799acee
Milind Mishra <milind.mishra4@gmail.com>
2024-11-06 08:10:18 +0530

    feat: add learn more popup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { LearnMorePopup } from "@/components/learn-more-popup";
 import Providers from "@/lib/providers";
 import type { Metadata } from "next";
 import { Raleway } from "next/font/google";
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={raleway.className} suppressHydrationWarning>
+        <LearnMorePopup />
         <Providers>{children}</Providers>
       </body>
     </html>
